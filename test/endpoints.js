43e1c0bd8c63145be5b3b04ddf010ad835a8a294
Silas Getachew <silogecho@gmail.com>
2021-07-30 09:07:38 +0300

    test : api test start

--- test/endpoints.js
+++ test/endpoints.js
@@ -15,3 +15,14 @@ test.serial.cb('healthcheck', function (t) {
     t.end()
   })
 })
+
+test('not found', function (t) {
+  var url = '/404'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.ifError(err, 'no error')
+
+    t.equal(res.statusCode, 404, 'correct statusCode')
+    t.equal(res.body.error, 'Resource Not Found', 'error match')
+    t.end()
+  })
+})
