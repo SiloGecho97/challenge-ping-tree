9a9ff79e9d913534a78b7e82b6da69901251aa2f
Silas Getachew <silogecho@gmail.com>
2021-07-30 14:02:15 +0300

    test : get targets api test

--- test/endpoints.js
+++ test/endpoints.js
@@ -2,7 +2,8 @@ process.env.NODE_ENV = 'test'
 
 var test = require('ava')
 var servertest = require('servertest')
-
+const { getTargets } = require('../lib/controllers/target.controller')
+// const redis = require('../lib/redis/index')
 var server = require('../lib/server')
 
 test.serial.cb('healthcheck', function (t) {
@@ -16,13 +17,35 @@ test.serial.cb('healthcheck', function (t) {
   })
 })
 
-test('not found', function (t) {
-  var url = '/404'
-  servertest(server(), url, { encoding: 'json' }, function (err, res) {
-    t.ifError(err, 'no error')
+test('async/await support', async t => {
+  const value = await Promise.resolve(true);
+  var url = '/api/targets'
+  t.true(value)
+});
 
-    t.equal(res.statusCode, 404, 'correct statusCode')
-    t.equal(res.body.error, 'Resource Not Found', 'error match')
+
+test.serial.cb('Get targets', function (t) {
+  console.log(t.context.data)
+  var url = '/api/targets'
+  getTargets().then(db_data => {
+    servertest(server(), url, { encoding: 'json' }, function (err, res) {
+      t.falsy(err, 'no error')
+      t.is(res.statusCode, 200, 'correct statusCode')
+      t.deepEqual(res.body.data, db_data, 'status is ok')
+      t.end()
+    })
+  })
+
+})
+
+test.serial.cb('not found', function (t) {
+  var url = '/notfound'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.is(err, null, 'no error')
+
+    t.is(res.statusCode, 404)
+    t.is(res.body.error, 'Resource Not Found')
     t.end()
   })
 })
+
