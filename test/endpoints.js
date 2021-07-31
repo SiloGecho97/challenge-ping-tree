4aab06ba4c5d4fb707dc7acd9a3e3afa02c9bcd9
Silas Getachew <silogecho@gmail.com>
2021-07-31 11:36:31 +0300

    test : route test

--- test/endpoints.js
+++ test/endpoints.js
@@ -4,7 +4,8 @@ var test = require('ava')
 var servertest = require('servertest')
 const { getTargets } = require('../lib/controllers/target.controller')
 var server = require('../lib/server')
-
+const Stream = require('stream')
+const Targets = require('../lib/models')
 test.serial.cb('healthcheck', function (t) {
   var url = '/health'
   servertest(server(), url, { encoding: 'json' }, function (err, res) {
@@ -28,8 +29,13 @@ test.serial.cb('Get targets', function (t) {
   })
 })
 
-test.serial.cb('Post targets', function (t) {
-  var url = '/api/targets'
+
+
+test.serial.cb('should get values by id', function (t) {
+  var post_url = '/api/targets'
+  var post_opts = { method: 'POST', encoding: 'json' }
+  var get_url = '/api/target'
+  var get_opts = { method: 'GET', encoding: 'json' }
   const target = {
     url: 'http://targets.com',
     value: '0.50',
@@ -44,18 +50,81 @@ test.serial.cb('Post targets', function (t) {
     }
   }
 
-  const serverStream = servertest(server(), url, { encoding: 'json', method: 'POST' })
-  serverStream._write(JSON.stringify(target))
-  serverStream.on('end', (_err, data) => {
-    // t.falsy(err, 'no error')
-    t.is(data.statusCode, 200, 'correct statusCode')
-    // t.deepEqual(res.body.data, 'dbData', 'status is ok')
-    t.end()
-  })
-  serverStream.on('data', (_err, data) => {
-    console.log(data)
-  })
+  servertest(server(), post_url, post_opts, onResponse)
+    .end(JSON.stringify(target))
+
+  function onResponse(err, res) {
+    // t.ifError(err, 'no error')
+    t.is(res.statusCode, 200, 'correct statusCode')
+    servertest(server(), `${get_url}/${res.body.data.id}`, get_opts, (err, resp) => {
+      t.is(JSON.stringify(resp.body.data), JSON.stringify(resp.body.data), 'Correct Data')
+      t.end()
+    })
+  }
 })
+
+/**
+ * Test Get by Id and Post
+ */
+test.serial.cb('should post values', function (t) {
+  var url = '/api/targets'
+  var opts = { method: 'POST', encoding: 'json' }
+  const target = {
+    url: 'http://targets.com',
+    value: '0.50',
+    maxAcceptsPerDay: '10',
+    accept: {
+      geoState: {
+        $in: ['ca', 'ny']
+      },
+      hour: {
+        $in: ['13', '14', '15']
+      }
+    }
+  }
+  t.plan(1)
+  servertest(server(), url, opts, onResponse)
+    .end(JSON.stringify(target))
+
+  function onResponse(err, res) {
+    // t.ifError(err, 'no error')
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.end()
+    const Target = new Targets()
+    Target.getTargetCallback(res.body.data.id, (data) => {
+      // t.is(JSON.stringify(res.body.data), JSON.stringify(data), 'Correct Data')
+
+    })
+  }
+})
+
+/**
+ * Test Get by Id and Post
+ */
+test.serial.cb('Route decision', function (t) {
+  var url = '/route'
+  var opts = { method: 'POST', encoding: 'json' }
+  const postData = {
+    geoState: "ny",
+    publisher: "abc",
+    timestamp: "2018-07-19T23:28:59.513Z"
+  }
+
+  t.plan(1)
+  servertest(server(), url, opts, onResponse)
+    .end(JSON.stringify(postData))
+
+  function onResponse(err, res) {
+    // t.ifError(err, 'no error')
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(err, 200, 'correct statusCode')
+
+    t.end()
+
+
+  }
+})
+
 
 test.serial.cb('not found', function (t) {
   var url = '/notfound'
